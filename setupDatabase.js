82c32e427277acc78bc642be3bdac53cb79210a9
Oleh Lutsenko <pompolut@gmail.com>
2020-09-15 21:20:58 +0200

    change db file location

--- setupDatabase.js
+++ setupDatabase.js
@@ -2,7 +2,7 @@ const { open } = require('sqlite');
 const sqlite3 = require('sqlite3');
 
 async function setup() {
-    const db = await open({ filename: './test-db.sqlite', driver: sqlite3.Database });
+    const db = await open({ filename: './public/db/test-db.sqlite', driver: sqlite3.Database });
     await db.migrate({ force: true });
 
     const data = await db.all('SELECT * FROM migrations');
